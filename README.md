95f819f08d998878e6f28e5d11b54b45cda2fb06
web-mech <webmech@gmail.com>
2023-11-10 13:07:40 -0500

    feat(*): add full proof-of-concept addressing all WS connection concerns

--- README.md
+++ README.md
@@ -1,3 +1,100 @@
-# ws-api-client
+# TheBlock Priority API WebSocket Client
 
-A simple client for connecting to the Priority API
+This project is a WebSocket client for the TheBlock Priority API service. It provides real-time updates on various cryptocurrencies and blockchain-related news.
+
+## Features
+
+- Real-time news updates via WebSocket.
+- Automatic reconnection and authentication handling.
+- Stores authentication token for persistent connections.
+- Optional filtering for specific tokens or priority levels in news updates.
+
+## Requirements
+
+- Node.js (16+)
+- WebSocket (`ws` package)
+- Filesystem access (`fs` package)
+
+### API Key Generation
+- An API key is necessary to access TheBlock's news feed.
+- You can generate this key by visiting [TheBlock API Key Generation](https://www.theblock.pro/api-public/).
+- Follow the instructions on the website to create and retrieve your API key.
+
+
+## Installation
+
+Clone the repository and install dependencies:
+
+```bash
+git clone [repository-url]
+cd [repository-name]
+yarn install
+```
+
+## Usage
+
+```bash
+yarn start
+```
+
+> On first run, the script will prompt for an email and API key. These credentials are used to authenticate with TheBlock Pro API service and receive an authentication token.
+
+## Configuration
+
+### API Key and Email
+- The script requires an API key and email for authentication.
+- If the `ws-api-key.txt` file is empty, the script will prompt you to enter these details.
+- The script will then automatically authenticate with TheBlock's auth endpoint and store the token for future use.
+
+### WebSocket Endpoint
+- The default WebSocket endpoint is set to receive all news without filters.
+- You can modify the endpoint in the script to apply filters for specific tokens or priority levels.
+
+### Connection Handling
+- The script includes automatic reconnection logic.
+- A heartbeat is sent every 30 seconds to keep the connection alive.
+- If the connection is lost, the script attempts to reconnect.
+
+## Events
+
+### Open Event
+- Triggered when the WebSocket connection is successfully established.
+- Logs a confirmation message with the connected endpoint.
+
+### Ping Event
+- Handles incoming pings from the server.
+- Logs the ping time for monitoring latency.
+
+### Message Event
+- Triggered on receiving a new message (news update).
+- Logs the received payload for real-time updates.
+
+### Close Event
+- Triggered when the WebSocket connection is closed.
+- Logs the closure code and reason.
+
+
+## Error Handling
+
+- The script includes error handling for WebSocket events.
+- If a `401` status code is encountered, the script re-authenticates using the last used email and API key.
+
+## Troubleshooting
+
+- **Connection Issues**: If you face connection issues, check your internet connection and the WebSocket endpoint URL.
+- **Authentication Failure**: If authentication fails, ensure your API key and email are correct. The script will re-authenticate automatically on a `401` error.
+- **WebSocket Errors**: For errors related to WebSocket, refer to the [WebSocket documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket) for troubleshooting.
+
+
+## Contributing
+
+- Contributions to improve the script or documentation are welcome.
+- Please follow the project's coding standards and submit pull requests for review.
+
+
+## License
+
+- This project is open source and available under the [MIT License](https://opensource.org/licenses/MIT).
+
+For more information or assistance, feel free to open an issue in the project repository.
+
--- ws-api-client.js
+++ ws-api-client.js
@@ -1,60 +1,104 @@
-const WebSocket = require('ws')
 const fs = require('fs')
+const readline = require('readline')
+const axios = require('axios')
+const WebSocket = require('ws')
 
+const AUTH_ENDPOINT = 'https://www.theblock.pro/api-public/v1/users/auth'
 //const ENDPOINT = 'wss://www.theblock.pro/api-public/v1/news/live?tokens=btc,eth,aave,ada,algo,bnb,bch,eos,luna' //token filter
 //const ENDPOINT = 'wss://www.theblock.pro/api-public/v1/news/live?priority=1,2,3' //priority filter
-const ENDPOINT =  'wss://www.theblock.pro/api-public/v1/news/live' //no filter
-const AUTH_ENDPOINT = 'https://www.theblock.pro/api-public/v1/auth'
-
+const ENDPOINT = 'wss://www.theblock.pro/api-public/v1/news/live'
 const LAST_PING_TIMEOUT = 60000
 
-// Read the key from the text file
-const key = fs.readFileSync('./ws-api-key.txt', 'utf-8').trim()
+const read = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-function time() {
-  const cliGreenCode = '\x1b[32m'
-  return [cliGreenCode, new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), '\x1b[0m'].join('')
+let credentials = { email: '', apiKey: '' }
+
+// Function to authenticate and get token
+async function authenticateAndGetToken(email, apiKey) {
+  try {
+    const response = await axios.post(AUTH_ENDPOINT, { email, apiKey })
+    if (response.status === 200 && response.data.data.token) {
+      return response.data.data.token
+    }
+    throw new Error('Authentication failed')
+  } catch (error) {
+    console.error('Error in authentication:', error.message)
+    process.exit(1)
+  }
 }
 
-// Connect to the WebSocket
-function connect() {
+// Function to read the API key from file or get it from the user
+async function readApiKey() {
+  try {
+    const key = fs.readFileSync('./ws-api-key.txt', 'utf-8').trim()
+    if (!key) throw new Error('Empty key file')
+    return key
+  } catch (error) {
+    return new Promise((resolve) => {
+      read.question('Enter your email: ', email => {
+        read.question('Enter your API key: ', apiKey => {
+          credentials = { email, apiKey }
+          authenticateAndGetToken(email, apiKey)
+            .then(token => {
+              fs.writeFileSync('./ws-api-key.txt', token)
+              resolve(token)
+            })
+        })
+      })
+    })
+  }
+}
+
+
+
+
+function time() {
+  return `\x1b[32m${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}\x1b[0m`
+}
+
+async function connect() {
   let lastPing = Date.now()
+  const key = await readApiKey()
 
   const ws = new WebSocket(ENDPOINT, {
     headers: {
       'x-auth-token': key
     }
   })
-  
+
   ws.on('open', function open() {
     console.log('Connected to', ENDPOINT)
-  
-    // Send a heartbeat every 30 seconds
-    let lastSetInterval = setInterval(() => {
+    setInterval(() => {
       if (ws.readyState !== WebSocket.OPEN || Date.now() - lastPing > LAST_PING_TIMEOUT) {
         console.log('Connection lost, reconnecting...')
         ws.terminate()
-        clearInterval(lastSetInterval)
         connect()
       }
     }, 30000)
   })
-  
-  ws.on('ping', function incoming() {
-    // Calculate the ping time
-    const pingTime = Date.now() - lastPing;
-    lastPing = Date.now()
-    const datetimeStringwithMilliseconds = 
 
+  ws.on('ping', function incoming() {
+    const pingTime = Date.now() - lastPing
+    lastPing = Date.now()
     console.log(`[${time()}] Ping:`, pingTime)
   })
 
   ws.on('message', function incoming(data) {
     console.log(`[${time()}] PAYLOAD:`, data.toString())
   })
-  
-  ws.on('close', function close(code, reason) {
-    console.log(`Disconnected (code: ${code}, reason: ${reason})`)
+
+  ws.on('close', async function close(code, reason) {
+    if (code === 401 && credentials.email && credentials.apiKey) {
+      console.log('Re-authenticating due to 401 Unauthorized error...')
+      const newToken = await authenticateAndGetToken(credentials.email, credentials.apiKey)
+      fs.writeFileSync('./ws-api-key.txt', newToken)
+      connect()
+    } else {
+      console.log(`Disconnected (code: ${code}, reason: ${reason})`)
+    }
   })
 }
 
